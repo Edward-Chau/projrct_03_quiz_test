89cc68015b87ea559c207e26229fdb256da7db95
Edward-Chau <edwardchau517@gmail.com>
2024-09-29 23:46:13 +0800

    01

--- lib/question_list/hard.dart
+++ lib/question_list/hard.dart
@@ -9,9 +9,9 @@ final List<Question> hardQuestion = [
   Question(
       question: 'How are Flutter UIs built?',
       answer: [
-        'By combining widgets in code',
-        'By combining widgets in a visual editor',
-        'By defining widgets in config files',
+        'By combining widgets in code.',
+        'By combining widgets in a visual editor.',
+        'By defining wridgets in config files',
         'By using XCode for iOS and Android Studio for Android',
       ],
       correct: 'combining')
